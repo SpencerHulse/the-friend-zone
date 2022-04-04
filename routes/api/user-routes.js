46b3c74668aa14bc9d792a25ff0203826dec13f5
Spencer Hulse <hulse.spencer@gmail.com>
2022-04-03 23:16:36 -0400

    Finished user routes, including friend routes.

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,10 +1,21 @@
 const router = require("express").Router();
 const {
   getAllUsers,
+  getOneUserById,
   createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
 } = require("../../controllers/user-controller");
 
 // Route - /api/users
 router.route("/").get(getAllUsers).post(createUser);
+
+// Route - /api/users/:userId
+router.route("/:userId").get(getOneUserById).put(updateUser).delete(deleteUser);
+
+// Route - /api/users/:userId/friends/:friendId
+router.route("/:userId/friends/:friendId").put(addFriend).delete(removeFriend);
 
 module.exports = router;
